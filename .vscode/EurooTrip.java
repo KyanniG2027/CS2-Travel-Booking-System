c4caa8cfa8a92a717815c0131951e159476fc7b9
Kyanni <kyanni.gonzalez2027@gbwl.org>
2026-01-22 19:26:10 +0000

    left off on parameterized consturctors

---
+++ .vscode/EurooTrip.java
@@ -0,0 +1,69 @@
+public class EurooTrip{
+    //INSTANCE VARIABlES (data)
+    private static int procesFee = 1000;
+    private  String TravelerName;
+    private int durationInDays;
+    private boolean fivestarHotel;    
+    private double luggageWeight;
+    private String destinations; 
+    
+    //CONSTRUCTORS (defult + parameterized)
+    public EurooTrip(){
+        this.TravelerName = "DefaultClient"; 
+        this.durationInDays= 14; 
+        this.fivestarHotel= yes;
+        this.luggageWeight= 80; 
+        this.destinations= "Europe Trip🛳️🗺️🇪🇸🌟";
+    }
+    // PARAMATERIZED CONSTRUCTORS
+    public EurooTrip (String s, int t, boolean b, double d, String p ){
+        this.TravelerName = s; 
+        this.durationInDays = t;
+        this.fivestarHotel = b;
+        this.luggageWeight = d;
+        this.destinations = p;
+    }
+   
+   public int getprocesFee(){
+         return procesFee;
+
+    public String getTravlerName(){
+        return getTravlerName;
+    }
+    public int getdurationInDays(){
+        return durationInDays;
+    }
+
+    public boolean getfivestarHotel()[
+        return fivestarHotel;
+    ]
+    
+    public double getluggageWeight(){
+        return luggageWeight;
+    }
+    public String getdestinations(){
+        return destintations; 
+    }
+  
+  
+  
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
